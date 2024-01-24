dc27455afb7f204857a66301f91488be18d9cc12
kweon001128 <kweon001128@naver.com>
2024-01-25 05:47:41 +0900

    .

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-
-
-/* 페이지1 gsap*/
-const tl1 = gsap.timeline();
-
-tl1.from(
-"h1",{
-  repeat:-1,
-  repeatDelay:1,
+/* 페이지 1 시작*/
+gsap.from('.f-p-g',{
+  y:800,
   opacity:0.5,
+  duration:1.5,
 });
 
-tl1.from(".page-1 > div > .tl1", { duration: 0.2, y: -200, opacity: 0 });
+const tl1 = gsap.timeline();
+
+tl1.from(".page-1 > div > .tl1", { duration: 0.2, y: -200, opacity: 0 },"+=1.5");
+
 tl1.from(
   ".page-1 > div > .tl2",
   { duration: 0.2, y: -200, opacity: 0 },
@@ -33,22 +31,43 @@ tl1.from(
 );
 
 tl1.from(
-  ".page-1 > .line-4 > footer",
-  {
-    duration: 0.5, opacity: 0
-  },
-  "+=0.5"
-)
+".line-4",{
+  opacity:0,
+  duration:0.8,
+},"+=0.3")
 
 tl1.from(
-".page-1 > .line-4 > i",{
-  repeat:3,
-  repeatDelay:1.5,
-  y:-100,
-  opacity:0,
-},"+=0.3");
+  ".page-1 > .line-4 > span > i",
+  {
+    repeat: 3,
+    repeatDelay: 1.5,
+    y: -150,
+    opacity: 0
+  },
+  "+=0.2"
+);
 
-gsap.from(".page-1", {
+const p2t = document.querySelector(".page-2").offsetTop
+
+$('.page1__btn-box-1').click(function(){
+  gsap.to(window,{
+    duration:0.5, scrollTo: p2t
+  })
+});
+
+/* 페이지 1 끝 페이지 2 시작*/
+/*gsap.from('.c-p-g',{
+  y:100,
+  scale:0.8,
+  opacity:0.8,
+  duration:0.3,
+  scrollTrigger:{
+    trigger:'.page-2',
+    toggleAction: 'start none none none',
+  },
+});
+
+/*gsap.from(".page-1", {
   scrollTrigger: {
     trigger: ".page-1",
     pin: true,
@@ -64,113 +83,4 @@ gsap.from(".page-2", {
     pinSpacing: false,
     start: "top top"
   }
-});
-/* 페이지1 gsap*/
-
-/* 페이지2 gsap 스크롤트리거*/
-/* 페이지2 gsap 스크롤트리거*/
-let W = window.innerWidth;
-let H = window.innerHeight;
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const maxConfettis = 150;
-const particles = [];
-
-const possibleColors = [
-  "DodgerBlue",
-  "OliveDrab",
-  "Gold",
-  "Pink",
-  "SlateBlue",
-  "LightBlue",
-  "Gold",
-  "Violet",
-  "PaleGreen",
-  "SteelBlue",
-  "SandyBrown",
-  "Chocolate",
-  "Crimson"
-];
-
-function randomFromTo(from, to) {
-  return Math.floor(Math.random() * (to - from + 1) + from);
-}
-
-function confettiParticle() {
-  this.x = Math.random() * W; // x
-  this.y = Math.random() * H - H; // y
-  this.r = randomFromTo(11, 33); // radius
-  this.d = Math.random() * maxConfettis + 11;
-  this.color =
-    possibleColors[Math.floor(Math.random() * possibleColors.length)];
-  this.tilt = Math.floor(Math.random() * 33) - 11;
-  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
-  this.tiltAngle = 0;
-
-  this.draw = function() {
-    context.beginPath();
-    context.lineWidth = this.r / 2;
-    context.strokeStyle = this.color;
-    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
-    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
-    return context.stroke();
-  };
-}
-
-function Draw() {
-  const results = [];
-
-  // Magical recursive functional love
-  requestAnimationFrame(Draw);
-
-  context.clearRect(0, 0, W, window.innerHeight);
-
-  for (var i = 0; i < maxConfettis; i++) {
-    results.push(particles[i].draw());
-  }
-
-  let particle = {};
-  let remainingFlakes = 0;
-  for (var i = 0; i < maxConfettis; i++) {
-    particle = particles[i];
-
-    particle.tiltAngle += particle.tiltAngleIncremental;
-    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
-    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
-
-    if (particle.y <= H) remainingFlakes++;
-
-    // If a confetti has fluttered out of view,
-    // bring it back to above the viewport and let if re-fall.
-    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
-      particle.x = Math.random() * W;
-      particle.y = -30;
-      particle.tilt = Math.floor(Math.random() * 10) - 20;
-    }
-  }
-
-  return results;
-}
-
-window.addEventListener(
-  "resize",
-  function() {
-    W = window.innerWidth;
-    H = window.innerHeight;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  },
-  false
-);
-
-// Push new confetti objects to `particles[]`
-for (var i = 0; i < maxConfettis; i++) {
-  particles.push(new confettiParticle());
-}
-
-// Initialize
-canvas.width = W;
-canvas.height = H;
-Draw();
-
-
+}); */
